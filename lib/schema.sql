c097ac19750064347775f6d8f99432244e74071c
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 10:34:40 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -6,6 +6,8 @@ subgenre_id INTEGER
 );
 
 
+
+
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
 INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
