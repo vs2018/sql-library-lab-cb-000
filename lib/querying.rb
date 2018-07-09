ad357587438802187fd4bec64b0bec76ee057840
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 11:24:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM characters INNER JOIN series ON characters.series_id = series.id GROUP BY COUNT(characters) ORDER BY COUNT(characters) DESC LIMIT 1"
+  "SELECT series.title FROM characters INNER JOIN series ON characters.series_id = series.id GROUP BY COUNT(characters.series_id) ORDER BY COUNT(characters.series_id) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
