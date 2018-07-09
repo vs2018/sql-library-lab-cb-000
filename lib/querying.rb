0eb8106c2e89fd898d690cc5d5bd3104cc1f9408
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 11:13:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT value, "
+  "SELECT species, COUNT(species) FROM characters ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
